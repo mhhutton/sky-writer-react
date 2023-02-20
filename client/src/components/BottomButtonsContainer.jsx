249260d8f0b90ee5d90eac9dbbda044b3edc9928
mhhutton <mhhutton@gmail.com>
2023-02-19 17:19:10 -0700

    Installed bootstrap. Added buttons.

--- client/src/components/BottomButtonsContainer.jsx
+++ client/src/components/BottomButtonsContainer.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
 
 function BottomButtonsContainer() {
   return (
     <div className="bott-butt-cont">
-      <div className="title-text">Buttons!</div>
+      <div className="title-text">
+        <Button>Create New Message</Button>
+        <Button> Fly Message</Button>
+      </div>
     </div>
   );
 }
